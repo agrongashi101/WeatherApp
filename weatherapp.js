bc1f7d6980de88e15f603d2e14b796ea2e8d10c6
agrongashi101 <agrongashi452@gmail.com>
2025-02-06 16:49:39 +0100

    Update weatherapp.js
    Update

--- weatherapp.js
+++ weatherapp.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "33e3dffda141646b764ffa8a0ca9038d";
+const apiKey = "your api key";
 
 
 weatherForm.addEventListener("submit", async event => {
